743c21f40238f65ab6482a9ba1071fa6bef47b02
Tonny <tonnyodera6967@gmail.com>
2023-11-17 16:40:18 +0300

    second commit

--- app/src/main/java/com/example/waterqua/HomeActivity.java
+++ app/src/main/java/com/example/waterqua/HomeActivity.java
@@ -1,11 +1,12 @@
 package com.example.waterqua;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,8 +14,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-        String username = sharedPreferences.getString("usename" , "").toString();
-        Toast.makeText(getApplicationContext(), "Hi  " +username + "Welcome " , Toast.LENGTH_SHORT).show();
+       // SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+       // String username = sharedPreferences.getString("usename" , "").toString();
+        Toast.makeText(getApplicationContext(), "Hi  " + "Welcome " , Toast.LENGTH_SHORT).show();
+
+        CardView waterNews = findViewById(R.id.cardWaternews);
+        waterNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, ArticleActivity.class));
+            }
+        });
+
     }
+
 }
